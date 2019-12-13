95ddafe641413cf7562841f240a4f786c90a1cdd
Piyush142 <chouhanpiyush142@gmail.com>
2019-12-14 02:49:33 +0530

    seventh change

--- imagenet_utils.py
+++ imagenet_utils.py
@@ -12,7 +12,8 @@ def preprocess_input(x, dim_ordering='default'):
     if dim_ordering == 'default':
 ## dim_ordering = K.image_dim_ordering()
 ##        dim_ordering = K.image_data_format()
-          dim_ordering = K.image_data_format()
+           dim_ordering = K.image_data_format()
+           dim_ordering
     assert dim_ordering in {'channels_last', 'channels_first'}
 
     if dim_ordering == 'channels_first':
