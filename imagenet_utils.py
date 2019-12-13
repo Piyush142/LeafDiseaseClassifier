fe9838cfd35c4f99dd76274b1a56719944530e5d
Piyush142 <chouhanpiyush142@gmail.com>
2019-12-13 23:32:38 +0530

    changes in imagenet1

--- imagenet_utils.py
+++ imagenet_utils.py
@@ -11,7 +11,8 @@ CLASS_INDEX_PATH = 'https://s3.amazonaws.com/deep-learning-models/image-models/i
 def preprocess_input(x, dim_ordering='default'):
     if dim_ordering == 'default':
 ## dim_ordering = K.image_dim_ordering()
-        dim_ordering = K.image_data_format()
+##        dim_ordering = K.image_data_format()
+          dim_ordering = K.set_image_data_format()
     assert dim_ordering in {'tf', 'th'}
 
     if dim_ordering == 'th':
